2c029a2c83c200af59e447a6a5acf5ea3fe83339
vido-bit <itsvido@gmail.com>
2021-11-17 15:09:05 +0100

    deleted home icon in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,7 +8,6 @@ export default function Header() {
         <img src="/logo.svg" alt="politiX" />
       </a>
       <div className="menu-ico">
-        <img src="/home.svg" alt="home" style={{ width: "100%" }} />
         <img src="/profile.svg" alt="profile" style={{ width: "100%" }} />
         <img src="/menu.svg" alt="menu" style={{ width: "100%" }} />
       </div>
