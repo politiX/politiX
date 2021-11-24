f43038e707f9e304fa2386d11b5264d35f46d436
Matej-Sulfrian <matej.sulfrian@hs-furtwangen.de>
2021-11-24 16:44:18 +0100

    change to jsx (swipe)

--- frontend/src/components/Swipe.jsx
+++ frontend/src/components/Swipe.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {useState} from "react"
+import {graphql} from "gatsby"
 import {Pagination, A11y} from 'swiper'
 import {Swiper, SwiperSlide} from "swiper/react";
 
