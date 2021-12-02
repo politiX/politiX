1e20da386f8e34828f6a7303bfc853cfa8aa0aa5
FlexiLue <felix.luette@web.de>
2021-12-02 02:04:21 +0100

    preview Component added

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { useState } from "react";
 import Layout from "../components/Layout";
+import Preview from "../components/Preview";
 import "../styles/global.css";
 import "../styles/swipe.css";
 import {
@@ -10,31 +12,32 @@ import {
 const GET_ALL_TIMELINES = gql`
   query Timelines($categoryTimelines: String!) {
     timelines(where: { category:{ titel: $categoryTimelines }}) {
-          preview
           id
           titel
-          category {
-             id,
-             titel
+          preview_picture {
+            formats
+          }
+          articles(limit: 1, sort:"date:desc"){
+            titel
+            preview
+            date
           }
     }
   }
   `;
 
 export default function Home() {
-
     let categoryTimelines = "Bildung";
-
+    
     const { loading, error, data, refetch } = useQuery(GET_ALL_TIMELINES, {
         variables: { categoryTimelines: categoryTimelines }
     });
 
     if (loading) return null;
     if (error) console.log(error);
-    if (data) console.log(data);
+    /* if (data) console.log(data) */
 
     const loadData = (category) => {
-        console.log(category)
 
         categoryTimelines = category
 
@@ -45,7 +48,7 @@ export default function Home() {
         <div>
             <Layout loadData={loadData}>
                 <h1>Timelines</h1>
-                <p>{data.timelines[0].preview}</p>
+                <Preview timelines={data.timelines}></Preview>
             </Layout>
         </div>
     );
