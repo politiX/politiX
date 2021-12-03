eb0dc34b04159ab0a4eb2a881495f53a203d6fa2
vido-bit <itsvido@gmail.com>
2021-12-03 09:38:49 +0100

    Styling Preview Img

--- frontend/src/components/Preview.jsx
+++ frontend/src/components/Preview.jsx
@@ -5,12 +5,12 @@ export default function Preview({timelines}) {
     const timelinesRender = timelines.map((timeline) =>
         <Link to={"/timelines/" + timeline.titel}>
             <div key={timeline.id}>
+                <div class="preview-text">
                 <h3>{timeline.titel}</h3>
-                <div>
                     <h2>{timeline.articles[0].titel}</h2> 
                     <p>{timeline.articles[0].preview}</p>
-                    <img src={timeline.preview_picture ? timeline.preview_picture.formats.thumbnail.url : ""} alt="" />
                 </div>
+                    <img class="preview-img" src={timeline.preview_picture ? timeline.preview_picture.formats.thumbnail.url : ""} alt="" />
             </div>
         </Link>
     )
