d294e8e3c8f00bfaa10670c8151e4d832130175e
vido-bit <itsvido@gmail.com>
2021-12-28 00:22:59 +0100

    scrollbar

--- frontend/src/components/Preview.jsx
+++ frontend/src/components/Preview.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import { Link } from "gatsby";
-import * as styles from '../styles/timelinePreview.module.css';
+import * as styles from "../styles/timelinePreview.module.css";
 
-export default function Preview({timelines}) {
-    console.log(timelines)
-    const timelinesRender = timelines.map((timeline) =>
-        <Link to={"/timelines/" + timeline.title}
-              key={timeline.id}
-        >
-            <div className={styles.preview}>
-                <h3>{timeline.title}</h3>
-                <div>
-                    <h2>{timeline.articles[0].title}</h2> 
-                    <p>{timeline.articles[0].preview}</p>
-                    <img src={timeline.preview_picture ? timeline.preview_picture.formats.medium.url : ""} alt="" />
-                </div>
-            </div>
-        </Link>
-    )
-    return (
-        <div>
-            {timelinesRender}
-        </div>
-    );
+export default function Preview({ timelines }) {
+  console.log(timelines);
+  const timelinesRender = timelines.map((timeline) => (
+    <Link to={"/timelines/" + timeline.title} key={timeline.id}>
+      <div className={styles.preview}>
+        <div className={styles.scrollbar}></div>
+        <div className={styles.scrollball}></div>
+        <h3>{timeline.title}</h3>
+        <h2>{timeline.articles[0].title}</h2>
+        <p>{timeline.articles[0].preview}</p>
+        <img
+          src={
+            timeline.preview_picture
+              ? timeline.preview_picture.formats.medium.url
+              : ""
+          }
+          alt=""
+        />
+      </div>
+    </Link>
+  ));
+  return <div>{timelinesRender}</div>;
 }
--- frontend/src/templates/timeline.jsx
+++ frontend/src/templates/timeline.jsx
@@ -1,62 +1,72 @@
 import React from "react";
 import Layout from "../components/Layout";
-import {graphql, navigate} from "gatsby";
-import * as styles from '../styles/timeline.module.css'
+import { graphql, navigate } from "gatsby";
+import * as styles from "../styles/timeline.module.css";
 
 export default function Timeline({ data }) {
+  // console.log(data.allStrapiTimeline.edges[0].node.articles)
 
-    // console.log(data.allStrapiTimeline.edges[0].node.articles)
+  const timelineTitle = data.allStrapiTimeline.edges[0].node.title;
 
-    const timelineTitle = data.allStrapiTimeline.edges[0].node.title
-
-    const articlePreviews = data.allStrapiTimeline.edges[0].node.articles.map((article) =>
-        <div className={styles.article_preview_w} key={article.id}>
-            <h2>{article.title}</h2>
-            <p>{article.preview}</p>
-        </div>
+  const articlePreviews = data.allStrapiTimeline.edges[0].node.articles.map(
+    (article) => (
+      <div className={styles.article_preview_w} key={article.id}>
+        <h2>{article.title}</h2>
+        <p>{article.preview}</p>
+      </div>
     )
+  );
 
-    // console.log(articlePreviews)
+  // console.log(articlePreviews)
 
-    const loadData = (category) => {
-        setTimeout(() => {
-            navigate(
-                "/",
-                {
-                    state: { category }
-                }
-            )
-        }, 300)
+  const loadData = (category) => {
+    setTimeout(() => {
+      navigate("/", {
+        state: { category },
+      });
+    }, 300);
+  };
 
-    }
+window.onscroll = function () {triggerScroll()};
 
-    return (
-        <div className={styles.timeline}>
-        <Layout loadData={loadData} category={data.allStrapiTimeline.distinct[0]}>
-
-            <h1>
-                {timelineTitle}
-            </h1>
-            {articlePreviews}
-        </Layout>
-        </div>
-    )
+  return (
+    <div className={styles.timeline}>
+      <Layout loadData={loadData} category={data.allStrapiTimeline.distinct[0]}>
+        <h1>{timelineTitle}</h1>
+        <div className={styles.scrollbar}></div>
+        {/* <div className={styles.scrollball}></div> */}
+        {articlePreviews}
+      </Layout>
+    </div>
+  );
 }
 
-export const query = graphql `
-    query MyQuery($timeline: String) {
-        allStrapiTimeline(filter: {title: {eq: $timeline}}) {
-            edges {
-                node {
-                    title
-                    articles {
-                        id
-                        title
-                        preview
-                    }
-                }
-            }
-            distinct(field: category___title)
+function triggerScroll(){
+  let timelinePreview = document.getElementsByClassName(
+    "timeline-module--article_preview_w--9k4FG"
+  );
+  for (let i = 0; i < timelinePreview.length; i++) {
+    // Hier sollte die ScrollHöhe der PreviewArticle erfasst werden
+    console.log(timelinePreview[i].scrollTop);
+    // console.log(window.scrollY);
+  }
+}
+
+
+export const query = graphql`
+  query MyQuery($timeline: String) {
+    allStrapiTimeline(filter: { title: { eq: $timeline } }) {
+      edges {
+        node {
+          title
+          articles {
+            id
+            title
+            preview
+          }
         }
+      }
+      distinct(field: category___title)
     }
-`
+  }
+`;
