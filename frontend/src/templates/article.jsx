5eb4c2c36c54bf2722a9511961c3eb152bec840a
vido-bit <itsvido@gmail.com>
2022-01-09 12:29:01 +0100

    article styles

--- frontend/src/templates/article.jsx
+++ frontend/src/templates/article.jsx
@@ -1,89 +1,116 @@
 import React from "react";
 import Layout from "../components/Layout";
-import {graphql, navigate} from "gatsby";
-import {Link} from "gatsby";
+import { graphql, navigate } from "gatsby";
+import { Link } from "gatsby";
 import * as styles from "../styles/article.module.css";
+import { isConstValueNode } from "graphql";
 
-export default function Article({data, location}) {
-    console.log(data.allStrapiArticle.edges[0].node.micro_article_container)
-    console.log(location.state.category)
+export default function Article({ data, location }) {
+  // console.log(data.allStrapiArticle.edges[0].node.micro_article_container);
+  // console.log(location.state.category);
 
-    const articleTitle = data.allStrapiArticle.edges[0].node.title;
-    const articleDate = data.allStrapiArticle.edges[0].node.published_at;
+  const articleTitle = data.allStrapiArticle.edges[0].node.title;
+  const articleDate = data.allStrapiArticle.edges[0].node.published_at;
 
-    let direction = 'right'
-    let end
-    const indicator = function (i) {
-        if ((i % 3) === 0) {
-            if (direction === 'right') {
-                direction = 'left'
-            } else {
-                direction = 'right'
-            }
-        }
-
-        if ((i+1)%3 === 0) {
-            end = 'end'
-        } else {
-            end = ''
-        }
-
-        return direction
+  let direction = "right";
+  let end;
+  const indicator = function (i) {
+    if (i % 3 === 0) {
+      if (direction === "right") {
+        direction = "left";
+      } else {
+        direction = "right";
+      }
     }
 
-    const articleDetail = data.allStrapiArticle.edges[0].node.micro_article_container.map(
-        (article, index) => (
-            <div className={'micro_w ' + indicator(index)} key={article.id}>
-                <div className='img_w'>
-                    <img src={
-                        article.image
-                            ? article.image.url
-                            : ""
-                    }
-                         alt=""/>
-                    <div className={'imgStick ' + end}></div>
-                </div>
-                <p>{article.text}</p>
-            </div>
+    if ((i + 1) % 3 === 0) {
+      end = "end";
+    } else {
+      end = "";
+    }
 
-        )
-    );
+    return direction;
+  };
 
-    // console.log(articlePreviews)
-
-    const loadData = (category) => {
-        setTimeout(() => {
-            navigate("/", {
-                state: {category},
-            });
-        }, 300);
-    };
-
-    return (
-        <div className={styles.timeline}>
-            <Layout loadData={loadData} category={location.state.category}>
-                <h1 className={styles.topBar}>{articleTitle}</h1>
-                <div className={styles.art_w}>
-                    {articleDetail}
-                    <p className={styles.date}>{articleDate}</p>
-                </div>
-            </Layout>
+  const articleDetail =
+    data.allStrapiArticle.edges[0].node.micro_article_container.map(
+      (article, index) => (
+        <div className={"micro_w " + indicator(index)} key={article.id}>
+          <div className="img_w">
+            <img src={article.image ? article.image.url : ""} alt="" />
+          </div>
+          <div className={"imgStick " + end}></div>
+          <p>{article.text}</p>
         </div>
+      )
     );
-}
 
+  // console.log(articlePreviews)
+
+  const loadData = (category) => {
+    setTimeout(() => {
+      navigate("/", {
+        state: { category },
+      });
+    }, 300);
+  };
+
+  if (typeof window !== undefined) {
+    window.onscroll = function () {
+      fitImgStick();
+    };
+  }
+  function fitImgStick() {
+    let articleELements = document.querySelectorAll(".micro_w");
+    let imgSticks = document.querySelectorAll(".imgStick");
+    // let iconDots = document.querySelectorAll(".img_w");
+    // for (let i = 0; i < articleELements.length - 1; i++) {
+    //   let stickLength =
+    //     iconDots[i + 1].offsetTop + iconDots[i].offsetTop;
+    //     // imgSticks[i].setAttribute("style", "top:" + stickLength / 2 + "px");
+    //     imgSticks[i].setAttribute("style", "height:" + stickLength + "px;");
+    //   console.log(iconDots[i + 1].offsetTop);
+    //   console.log(iconDots[i].offsetTop);
+    //   console.log(iconDots[i]);
+    // }
+    let lastELement = articleELements[articleELements.length - 1];
+    let lastImgStick = lastELement.children[1];
+    //  = lastIcon.children[1];
+    if (!lastImgStick.classList.contains("end")) {
+      lastImgStick.classList.add("end");
+    }
+  }
+  return (
+    <div className={styles.timeline}>
+      <Layout loadData={loadData} category={location.state.category}>
+        <div className={styles.topBar}>
+          <h1>{articleTitle}</h1>
+          {/* Hier muss noch der Link gebaut werden um zurück zur betreffenden Timeline zu kommen */}
+          {/* <Link to={timeline.title} key={timeline.id}> */}
+          <div className={styles.closeArticle}></div>
+          {/* </Link> */}
+        </div>
+        <div className={styles.sideBar}></div>
+        <div className={styles.art_w}>
+          {articleDetail}
+          <p className={styles.date}>{articleDate}</p>
+        </div>
+      </Layout>
+    </div>
+  );
+}
 
 export const query = graphql`
-query MyQuery2($article: String) {
-    allStrapiArticle(filter: {title: {eq: $article}}) {
-        edges {
-            node {
-                id
-                micro_article_container
-                published_at(fromNow: false, formatString: "DD.MMMM.YYYY")
-                title
-            }
+  query MyQuery2($article: String) {
+    allStrapiArticle(filter: { title: { eq: $article } }) {
+      edges {
+        node {
+          id
+          micro_article_container
+          published_at(fromNow: false, formatString: "DD.MMMM.YYYY")
+          title
         }
+      }
     }
-}
+  }
 `;
--- frontend/src/templates/timeline.jsx
+++ frontend/src/templates/timeline.jsx
@@ -1,131 +1,140 @@
 import React from "react";
 import Layout from "../components/Layout";
-import {graphql, navigate} from "gatsby";
-import {Link} from "gatsby";
+import { graphql, navigate } from "gatsby";
+import { Link } from "gatsby";
 import * as styles from "../styles/timeline.module.css";
 
-export default function Timeline({data}) {
-    // console.log(data.allStrapiTimeline.edges[0].node.articles)
+export default function Timeline({ data }) {
+  // console.log(data.allStrapiTimeline.edges[0].node.articles)
 
-    const timelineTitle = data.allStrapiTimeline.edges[0].node.title;
+  const timelineTitle = data.allStrapiTimeline.edges[0].node.title;
 
-    let str = 'active'
+  let str = "active";
 
-    const articlePreviews = data.allStrapiTimeline.edges[0].node.articles.map(
-        (article) => (
-            <Link to={"/timelines/" + timelineTitle + '/articles/' + article.title} key={article.id} state={{category: data.allStrapiTimeline.distinct[0]}}>
-                <div className={'article_preview_w ' + str} key={article.id} sign={article.id}>
-                    <div className={'dot ' + str} sign={article.id}>
-                        {str = ''}
-                    </div>
-                    <h2>{article.title}</h2>
-                    <p>{article.preview }</p>
-                    <p className='pub'>{article.published_at}</p>
-                </div>
-
-            </Link>
-        )
-    );
-
-    // console.log(articlePreviews)
-
-    const loadData = (category) => {
-        setTimeout(() => {
-            navigate("/", {
-                state: {category},
-            });
-        }, 300);
-    };
-
-
-    if (typeof window !== undefined) {
-        window.onscroll = function () {
-            triggerScroll();
-        };
-    }
-
-    function triggerScroll() {
-        let timelinePreview = document.querySelectorAll(
-            ".article_preview_w"
-        );
-        for (let i = 0; i < timelinePreview.length; i++) {
-            let triggerPoint
-            let sign = timelinePreview[i].getAttribute('sign')
-            let dot = document.querySelector('.dot[sign="' + sign + '"]')
-            let prevSign
-            let preDot
-            if (i === 0) {
-                triggerPoint = timelinePreview[i].offsetTop - window.scrollY
-                prevSign = sign
-                preDot = dot
-            } else {
-                triggerPoint = timelinePreview[i - 1].offsetTop - window.scrollY
-                prevSign = timelinePreview[i - 1].getAttribute('sign')
-                preDot = document.querySelector('.dot[sign="' + prevSign + '"]')
-            }
-
-            let scroll
-            if (typeof window !== undefined) {
-                scroll = window.pageYOffset
-            }
-
-
-            if (triggerPoint < 120) {
-                if (i !== 0) {
-                    preDot.classList.remove('active')
-                }
-                dot.classList.add('active')
-            } else if (dot.classList.contains('active') && i !== 0) {
-                dot.classList.remove('active')
-            }
-
-            timelinePreview[i].classList.remove('active')
-
-            if (scroll < 15) {
-                let sign = timelinePreview[0].getAttribute('sign')
-                let dot = document.querySelector('.dot[sign="' + sign + '"]')
-                dot.classList.add('active')
-            }
-
-
-        }
-
-        let els = document.querySelectorAll('.dot.active')
-        if (els.length > 0) {
-            if (els.length > 1) {
-                els[1].parentElement.classList.add('active')
-            } else {
-                els[0].parentElement.classList.add('active')
-            }
-        }
-
-    }
-
-    let height
-    if (document.readyStat) {
-        height = document.body.clientHeight - document.querySelector('.header').clientHeight - document.querySelector('footer').clientHeight - 10 + 'px'
-    } else {
-        height = 600 + 'px'
-    }
-    let windowHeight = {
-        height: height,
-        display: 'block'
-    }
-
-    return (
-        <div className={styles.timeline}>
-            <Layout loadData={loadData} category={data.allStrapiTimeline.distinct[0]}>
-                <h1>{timelineTitle}</h1>
-                <div className={styles.scrollbar}></div>
-                {/* <div className={styles.scrollball}></div> */}
-                {articlePreviews}
-            </Layout>
-
-            <div style={windowHeight}></div>
+  const articlePreviews = data.allStrapiTimeline.edges[0].node.articles.map(
+    (article) => (
+      <Link
+        to={"/timelines/" + timelineTitle + "/articles/" + article.title}
+        key={article.id}
+        state={{ category: data.allStrapiTimeline.distinct[0] }}
+      >
+        <div
+          className={"article_preview_w " + str}
+          key={article.id}
+          sign={article.id}
+        >
+          <div className={"dot " + str} sign={article.id}>
+            {(str = "")}
+          </div>
+          <h2>{article.title}</h2>
+          <p>{article.preview}</p>
+          <p className="pub">{article.published_at}</p>
         </div>
-    );
-}
+      </Link>
+    )
+  );
 
+  // console.log(articlePreviews)
+
+  const loadData = (category) => {
+    setTimeout(() => {
+      navigate("/", {
+        state: { category },
+      });
+    }, 300);
+  };
+
+  if (typeof window !== undefined) {
+    window.onscroll = function () {
+      triggerScroll();
+    };
+  }
+
+  function triggerScroll() {
+    let timelinePreview = document.querySelectorAll(".article_preview_w");
+    for (let i = 0; i < timelinePreview.length; i++) {
+      let triggerPoint;
+      let sign = timelinePreview[i].getAttribute("sign");
+      let dot = document.querySelector('.dot[sign="' + sign + '"]');
+      let prevSign;
+      let preDot;
+      if (i === 0) {
+        triggerPoint = timelinePreview[i].offsetTop - window.scrollY;
+        prevSign = sign;
+        preDot = dot;
+      } else {
+        triggerPoint = timelinePreview[i - 1].offsetTop - window.scrollY;
+        prevSign = timelinePreview[i - 1].getAttribute("sign");
+        preDot = document.querySelector('.dot[sign="' + prevSign + '"]');
+      }
+
+      let scroll;
+      if (typeof window !== undefined) {
+        scroll = window.pageYOffset;
+      }
+
+      if (triggerPoint < 120) {
+        if (i !== 0) {
+          preDot.classList.remove("active");
+        }
+        dot.classList.add("active");
+      } else if (dot.classList.contains("active") && i !== 0) {
+        dot.classList.remove("active");
+      }
+
+      timelinePreview[i].classList.remove("active");
+
+      if (scroll < 15) {
+        let sign = timelinePreview[0].getAttribute("sign");
+        let dot = document.querySelector('.dot[sign="' + sign + '"]');
+        dot.classList.add("active");
+      }
+    }
+
+    let els = document.querySelectorAll(".dot.active");
+    if (els.length > 0) {
+      if (els.length > 1) {
+        els[1].parentElement.classList.add("active");
+      } else {
+        els[0].parentElement.classList.add("active");
+      }
+    }
+  }
+
+  let height;
+  if (document.readyStat) {
+    height =
+      document.body.clientHeight -
+      document.querySelector(".header").clientHeight -
+      document.querySelector("footer").clientHeight -
+      10 +
+      "px";
+  } else {
+    height = 600 + "px";
+  }
+  let windowHeight = {
+    height: height,
+    display: "block",
+  };
+
+  return (
+    <div className={styles.timeline}>
+      <Layout loadData={loadData} category={data.allStrapiTimeline.distinct[0]}>
+        <div className={styles.topBar}>
+          <h1>{timelineTitle}</h1>
+          <div className={styles.closeArticle}></div>
+        </div>
+        <div className={styles.timelineBody}>
+          <div className={styles.scrollbar}></div>
+          {/* <div className={styles.scrollball}></div> */}
+          {articlePreviews}
+        </div>
+      </Layout>
+
+      <div style={windowHeight}></div>
+    </div>
+  );
+}
 
 export const query = graphql`
   query MyQuery($timeline: String) {
