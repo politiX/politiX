307e145c38dfa7676d849b5f0e6dc81344d5b0b0
vido-bit <itsvido@gmail.com>
2022-01-26 16:36:52 +0100

    added swiper arrows and improved desktop timeline design

--- frontend/src/components/Swipe.jsx
+++ frontend/src/components/Swipe.jsx
@@ -1,43 +1,45 @@
 import React from "react";
-import {Swiper, SwiperSlide} from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/grid";
-import "../styles/swipe.css"
+import "../styles/swipe.css";
 
 // import Swiper core and required modules
-import SwiperCore, {Grid} from "swiper";
+import SwiperCore, { Grid, Navigation } from "swiper";
 
 // install Swiper modules
 SwiperCore.use([Grid]);
+SwiperCore.use([Navigation]);
 
-export default function Swipe({categories, handleSwiperClick}) {
+export default function Swipe({ categories, handleSwiperClick }) {
+  const navItems = categories.map((category, index) => (
+    <SwiperSlide key={index}>
+      <p onClick={handleSwiperClick}>{category}</p>
+    </SwiperSlide>
+  ));
 
-    const navItems = categories.map((category, index) =>
-        <SwiperSlide key={index}>
-            <p onClick={handleSwiperClick}>{category}</p>
-        </SwiperSlide>
-    )
-
-    return (
-        <>
-            <Swiper
-                slidesPerView={3}
-                grid={{
-                    rows: 1,
-                }}
-                loop={true}
-                spaceBetween={30}
-                className="mySwiper"
-                clickable={0}
-                shortSwipes={false}
-                slideToClickedSlide={true}
-                simulateTouch={true}
-            >
-
-                {navItems}
-            </Swiper>
-        </>
-    );
+  return (
+    <>
+      <Swiper
+        slidesPerView={3}
+        grid={{
+          rows: 1,
+        }}
+        loop={true}
+        spaceBetween={30}
+        className="mySwiper"
+        clickable={0}
+        shortSwipes={false}
+        slideToClickedSlide={true}
+        simulateTouch={true}
+        navigation={true}
+        nextEl={".swiper-button-next"}
+        prevEl={".swiper-button-prev"}
+      >
+        {navItems}
+      </Swiper>
+    </>
+  );
 }
